6c50fed56f9a2d6a42733ce86c12754123fb563a
Juan Cruz Fortunatti <jfortunatti@paypal.com>
2020-08-07 15:38:09 -0300

    Adding TestBinaryGap to the index file

--- src/js/js-3.js
+++ src/js/js-3.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 /**
-    A binary gap within a positive integer N is any maximal sequence of consecutive zeros 
-    that is surrounded by ones at both ends in the binary representation of N.
-    For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
-    The number 529 has binary representation 1000010001 and contains two binary gaps: one of 
-    length 4 and one of length 3. The number 20 has binary representation 10100 and contains 
-    one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. 
+    A binary gap within a positive integer N is any maximal sequence of 
+    consecutive zeros that is surrounded by ones at both ends in the 
+    binary representation of N.
+
+    For example:
+    The number 9 has binary representation 1001 and contains a binary 
+    gap of length 2. 
+    The number 529 has binary representation 1000010001 and contains two 
+    binary gaps: one of length 4 and one of length 3. 
+    The number 20 has binary representation 10100 and contains one binary 
+    gap of length 1. 
+    The number 15 has binary representation 1111 and has no binary gaps. 
     The number 32 has binary representation 100000 and has no binary gaps.
 */
 
@@ -17,7 +23,7 @@ const binaryGap = decimalNumber => {
 const Fail = () => <span style={{ color: "red", marginLeft: 10 }}>FAIL</span>;
 const Pass = () => <span style={{ color: "green", marginLeft: 10 }}>PASS</span>;
 
-export const Test = () => {
+export const TestBinaryGap = () => {
   return (
     <div>
       <ul>
